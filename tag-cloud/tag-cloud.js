e7875ce93d6f810cd7353064dd176caf87ffbe90
Sean Zaferopolos <szafero@gmail.com>
2014-08-28 09:45:22 -0400

    Fix bad commit

--- tag-cloud/tag-cloud.js
+++ tag-cloud/tag-cloud.js
@@ -5,6 +5,6 @@
 		script.type = 'text/javascript';
 		document.getElementsByTagName('body')[0].appendChild(script);
 	};
-	load_to_body('http://szafero.github.io/dojoConfig.js');
-	load_to_body('http://szafero.github.io/dojo/dojo.js');
+	load_to_body('http://szafero.github.io/tag-cloud/dojoConfig.js');
+	load_to_body('http://szafero.github.io/tag-cloud/dojo/dojo.js');
 })();
